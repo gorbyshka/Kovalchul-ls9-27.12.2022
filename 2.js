18a0c3703a6ae6500943fed3b8ce7f9523e992a7
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2022-12-27 18:48:58 +0200

    completed 2task

---
+++ 2.js
@@ -0,0 +1,39 @@
+// 2
+//Написати функцію doMath(x, znak, y)
+// яка отримує 3 аргументи: числа x і y, рядок znak.
+// У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).
+//Вивести результат математичної дії, вказаної в змінній znak.
+//Обидва числа і знак виходять від користувача.
+
+const x = Number(prompt('Введите первое число'));
+const znak = (prompt('Введите знак'));
+const y = Number(prompt('Введите второе число'));
+let result;
+
+function doMath(x, znak, y) {
+    switch (znak) {
+        case '+':
+            result = x + y;
+            break;
+        case '-':
+            result = x - y;
+            break;
+        case '*':
+            result = x * y;
+            break;
+        case '/':
+            result = x / y;
+            break;
+        case '%':
+            result = x % y;
+            break;
+        case '^':
+            result = x ^ y;
+            break;
+        default:
+            alert('Error');
+    }
+    return alert(x + znak + y + '=' + result);
+}
+
+doMath(x, znak, y);
